bfdc0058e6fe4d9016f27874eae54c2ffa8e1f88
Maau <marenas981@gmail.com>
2024-04-04 17:19:28 -0600

    Container-fluid

--- pages/Contacto.php
+++ pages/Contacto.php
@@ -31,7 +31,7 @@
             </div>
         </nav>
 
-        <div class="container-xxl py-5 bg-color hero-header mb-3">
+        <div class="container-fluid py-5 bg-color hero-header mb-3">
             <div class="container my-5 py-5">
                 <div class="row align-items-center g-5">
                     <div class="col-lg-6 text-center text-lg-start">
